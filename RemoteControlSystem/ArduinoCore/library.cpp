322befd397b96f53b6ea12762a129f79fa335c92
Grigori Kartashyan <grigori.kartashyan@gmail.com>
2017-06-27 15:14:02 +0400

    Added core project for Arduino Uno

---
+++ RemoteControlSystem/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 4/25/2017 11:25:19 PM
+ * Author : Grigori
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
